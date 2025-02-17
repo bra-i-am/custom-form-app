f5de49ebe349e66db2971ed8734b5d8db9f515e1
Brayan Ceron <brayan.ceron@edunext.co>
2025-02-17 08:48:21 -0500

    refactor: add data authorization and employment situation fields, update error messages, and improve help text.

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -1,17 +1,31 @@
 from .models import ExtraInfo
 from django.forms import ModelForm
 
+
 class ExtraInfoForm(ModelForm):
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
+
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['favorite_movie'].error_messages = {
-            "required": u"Please tell us your favorite movie.",
-            "invalid": u"We're pretty sure you made that movie up.",
+        self.fields["favorite_movie"].error_messages.update(
+            {
+                "required": "Please tell us your favorite movie.",
+                "invalid": "We're pretty sure you made that movie up.",
+            }
+        )
+        self.fields["favorite_movie"].help_text = "Please tell us your favorite movie."
+
+        self.fields["data_authorization"].error_messages = {
+            "required": "Please authorize data processing"
         }
 
     class Meta(object):
         model = ExtraInfo
-        fields = ('favorite_editor', 'favorite_movie')
+        fields = [
+            "data_authorization",
+            "favorite_movie",
+            "employment_situation",
+            "allow_newsletter",
+        ]
--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 # Backwards compatible settings.AUTH_USER_MODEL
-USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
 
 
 class ExtraInfo(models.Model):
@@ -10,21 +10,39 @@ class ExtraInfo(models.Model):
     This model contains two extra fields that will be saved when a user registers.
     The form that wraps this model is in the forms.py file.
     """
-    user = models.OneToOneField(USER_MODEL, null=True)
-    FAVORITE_EDITOR = (
-        ('vim', 'Vim'),
-        ('emacs', 'Emacs'),
-        ('np', 'Notepad'),
-        ('cat', 'cat > filename'),
+
+    STUDENT = "Student"
+    UNEMPLOYED = "Unemployed"
+    PUBLIC_SERVICE_CONTRACT = "Public service contract"
+    PRIVATE_INSTITUTION_CONTRACT = "Private institution contract"
+    SELF_EMPLOYED = "Self employed entrepreneur"
+    OTHER = "Other"
+
+    EMPLOYMENT_SITUATION_CHOICES = [
+        (STUDENT, "Student"),
+        (UNEMPLOYED, "Unemployed"),
+        (PUBLIC_SERVICE_CONTRACT, "Public service contract"),
+        (PRIVATE_INSTITUTION_CONTRACT, "Private institution contract"),
+        (SELF_EMPLOYED, "Self employed entrepreneur"),
+        (OTHER, "Other"),
+    ]
+
+    user = models.OneToOneField(USER_MODEL, null=True, on_delete=models.CASCADE)
+
+    data_authorization = models.BooleanField(
+        verbose_name="I have read and understood the <a href='https://www.nau.edu.pt/legal/politica-de-privacidade/'"
+        "rel='noopener' target='_blank'>Privacy Policy</a>",
     )
 
-    favorite_movie = models.CharField(
-        verbose_name="Fav Flick",
-        max_length=100,
+    favorite_movie = models.CharField(verbose_name="Fav Flick", max_length=100)
+
+    employment_situation = models.TextField(
+        verbose_name="Employment situation",
+        choices=EMPLOYMENT_SITUATION_CHOICES,
     )
-    favorite_editor = models.CharField(
-        verbose_name="Favorite Editor",
-        choices=FAVORITE_EDITOR,
-        blank=True, 
-        max_length=5,
+
+    allow_newsletter = models.BooleanField(
+        verbose_name="I have read and agree to give my <a target='_blank' "
+        "href='https://www.nau.edu.pt/legal/consentimento-para-o-envio-de-"
+        "newsletters/'>consent to the sending of newsletters</a>",
     )
