379e44d2f5db1fd695bc2512e66fa0060331bbfa
Brayan Ceron <brayan.ceron@edunext.co>
2025-04-11 11:26:21 -0500

    refactor: enhance ExtraInfo model and form with additional fields and validation

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -1,31 +1,96 @@
-from .models import ExtraInfo
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.forms import ModelForm
+from .models import ExtraInfo
 
 
+# According to edx-platform FormDescription
+#
+# https://github.com/fccn/edx-platform/blob/315ca44d10d47bf4e16f0c916295c70939cf27e1/openedx/core/djangoapps/user_api/helpers.py#L95
+#
+# ALLOWED_TYPES = ["text", "email", "select", "textarea", "checkbox", "plaintext", "password", "hidden"]
+#
+# ALLOWED_RESTRICTIONS = {
+#     "text": ["min_length", "max_length"],
+#     "password": ["min_length", "max_length", "min_upper", "min_lower",
+#                     "min_punctuation", "min_symbol", "min_numeric", "min_alphabetic"],
+#     "email": ["min_length", "max_length", "readonly"],
+# }
+#
+# FIELD_TYPE_MAP = {
+#     forms.CharField: "text",
+#     forms.PasswordInput: "password",
+#     forms.ChoiceField: "select",
+#     forms.TypedChoiceField: "select",
+#     forms.Textarea: "textarea",
+#     forms.BooleanField: "checkbox",
+#     forms.EmailField: "email",
+# }
 class ExtraInfoForm(ModelForm):
     """
-    The fields on this form are derived from the ExtraInfo model in models.py.
+    Form that represents user extra info and is compatible with edX's FormDescription system.
     """
 
     def __init__(self, *args, **kwargs):
-        super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields["favorite_movie"].error_messages.update(
-            {
-                "required": "Please tell us your favorite movie.",
-                "invalid": "We're pretty sure you made that movie up.",
-            }
-        )
-        self.fields["favorite_movie"].help_text = "Please tell us your favorite movie."
+        super().__init__(*args, **kwargs)
 
-        self.fields["data_authorization"].error_messages = {
-            "required": "Please authorize data processing"
+        # Example: text field with restrictions
+        self.fields["full_name"].help_text = "Enter your full name."
+        self.fields["full_name"].min_length = 3
+        self.fields["full_name"].max_length = 100
+        self.fields["full_name"].restrictions = {
+            "min_length": 3,
+            "max_length": 100,
         }
 
-    class Meta(object):
+        # Textarea field
+        self.fields["bio"].widget = forms.Textarea()
+        self.fields["bio"].help_text = "Write a short bio."
+        self.fields["bio"].field_type = "textarea"
+
+        # Checkbox
+        self.fields["wants_newsletter"].label = "Subscribe to newsletter?"
+
+        # Select field
+        self.fields["favorite_language"].help_text = (
+            "Pick your preferred programming language."
+        )
+
+        # Plaintext hint
+        self.fields["password_hint"].help_text = (
+            "This will help you remember your password."
+        )
+
+        # Checkbox with custom message
+        self.fields["data_authorization"].error_messages = {
+            "required": "Please authorize data processing.",
+        }
+
+        # Agreement checkbox
+        self.fields["terms_accepted"].label = "I accept the Terms and Conditions"
+        self.fields["terms_accepted"].required = True
+
+    class Meta:
         model = ExtraInfo
         fields = [
             "data_authorization",
-            "favorite_movie",
-            "employment_situation",
-            "allow_newsletter",
+            "full_name",
+            "bio",
+            "wants_newsletter",
+            "favorite_language",
+            "password_hint",
+            "terms_accepted",
         ]
+
+        # According to edx-platform RegistrationFormFactory
+        #
+        # https://github.com/fccn/edx-platform/blob/315ca44d10d47bf4e16f0c916295c70939cf27e1/openedx/core/djangoapps/user_authn/views/registration_form.py#L457-L458
+        #
+        # here you have to use the same names as in the model and you can add default,
+        # field_type and include_default_option
+        serialization_options = {
+            "password_hint": {
+                "default": "Funny Password",
+            },
+        }
--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -1,48 +1,83 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import (
+    MinLengthValidator,
+    MaxLengthValidator,
+    RegexValidator,
+    EmailValidator,
+)
+from django.utils.translation import gettext_lazy as _
 
-# Backwards compatible settings.AUTH_USER_MODEL
 USER_MODEL = getattr(settings, "AUTH_USER_MODEL", "auth.User")
 
 
 class ExtraInfo(models.Model):
     """
-    This model contains two extra fields that will be saved when a user registers.
-    The form that wraps this model is in the forms.py file.
+    Enhanced model with additional fields demonstrating various field types and restrictions.
+    Fields are compatible with edx-platform FormDescription configuration.
     """
-
-    STUDENT = "Student"
-    UNEMPLOYED = "Unemployed"
-    PUBLIC_SERVICE_CONTRACT = "Public service contract"
-    PRIVATE_INSTITUTION_CONTRACT = "Private institution contract"
-    SELF_EMPLOYED = "Self employed entrepreneur"
-    OTHER = "Other"
-
-    EMPLOYMENT_SITUATION_CHOICES = [
-        (STUDENT, "Student"),
-        (UNEMPLOYED, "Unemployed"),
-        (PUBLIC_SERVICE_CONTRACT, "Public service contract"),
-        (PRIVATE_INSTITUTION_CONTRACT, "Private institution contract"),
-        (SELF_EMPLOYED, "Self employed entrepreneur"),
-        (OTHER, "Other"),
-    ]
 
     user = models.OneToOneField(USER_MODEL, null=True, on_delete=models.CASCADE)
 
     data_authorization = models.BooleanField(
-        verbose_name="I have read and understood the <a href='https://www.nau.edu.pt/legal/politica-de-privacidade/'"
-        "rel='noopener' target='_blank'>Privacy Policy</a>",
+        verbose_name=_(
+            "I have read and understood the <a href='https://www.nau.edu.pt/legal/politica-de-privacidade/'"
+            " rel='noopener' target='_blank'>Privacy Policy</a>"
+        )
     )
 
-    favorite_movie = models.CharField(verbose_name="Fav Flick", max_length=100)
-
-    employment_situation = models.TextField(
-        verbose_name="Employment situation",
-        choices=EMPLOYMENT_SITUATION_CHOICES,
+    full_name = models.CharField(
+        blank=True,
+        max_length=255,
+        validators=[MinLengthValidator(2)],
+        verbose_name=_("Full name"),
     )
 
-    allow_newsletter = models.BooleanField(
-        verbose_name="I have read and agree to give my <a target='_blank' "
-        "href='https://www.nau.edu.pt/legal/consentimento-para-o-envio-de-"
-        "newsletters/'>consent to the sending of newsletters</a>",
+    bio = models.TextField(
+        blank=True,
+        verbose_name=_("Biography"),
+        help_text=_("Tell us a bit about yourself."),
     )
+
+    wants_newsletter = models.BooleanField(
+        default=False,
+        verbose_name=_("Subscribe to newsletter"),
+    )
+
+    favorite_language = models.CharField(
+        blank=True,
+        max_length=50,
+        choices=[
+            ("python", "Python"),
+            ("javascript", "JavaScript"),
+            ("java", "Java"),
+            ("go", "Go"),
+        ],
+        verbose_name=_("Favorite programming language"),
+    )
+
+    password_hint = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name=_("Password hint"),
+        help_text=_("Optional hint to remember your password."),
+    )
+
+    terms_accepted = models.BooleanField(
+        default=False,
+        verbose_name=_("I accept the Terms and Conditions."),
+    )
+
+    registration_token = models.CharField(
+        blank=True,
+        max_length=64,
+        editable=False,
+        verbose_name=_("Registration token (internal)"),
+    )
+
+    def __str__(self):
+        return f"{self.full_name} <{self.email}>"
+
+    class Meta:
+        verbose_name = _("Extra Info")
+        verbose_name_plural = _("Extra Info entries")
